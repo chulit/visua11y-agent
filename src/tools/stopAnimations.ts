d336d289455375c9450244b5b1c49c60189369b3
Kholid Mawardi <kholidmawardi404@gmail.com>
2025-10-31 09:30:49 +0700

    fix: update demo page and stop animations tool
    - Updated demo/index.html with minor adjustments.
    - Applied necessary fixes/improvements to src/tools/stopAnimations.ts.

--- src/tools/stopAnimations.ts
+++ src/tools/stopAnimations.ts
@@ -4,12 +4,13 @@ import IToolConfig from '../types/IToolConfig';
 export const stopAnimationsConfig: IToolConfig = {
   id: 'stop-animations',
   selector: `html`,
-  childrenSelector: ['*'],
+  childrenSelector: ['*', '*::before', '*::after'],
   styles: {
     transition: 'none',
     'animation-fill-mode': 'forwards',
     'animation-iteration-count': '1',
     'animation-duration': '.01s',
+    'animation-play-state': 'paused',
   },
 };
 
